5029a290030baa797d3ae39b1d130e3a97756358
iacostap23 <iacostap@eafit.edu.co>
2025-11-09 15:19:35 -0500

    creación del servidor base

--- src/server.js
+++ src/server.js
@@ -1,12 +1,87 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+import os from "os"; 
+
+import arbitroRoutes from "./routes/arbitro.js";
+import authRoutes from "./routes/auth.js";
+import newsRoutes from "./routes/news.js";
+
+import swaggerUi from "swagger-ui-express"; 
+import swaggerSpec from "./swagger.js";     
+
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.json({ ok: true, message: "Servidor base funcionando " });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json({ limit: "2mb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
+
+
+app.use((req, res, next) => {
+  res.set("x-container-id", os.hostname()); 
+  next();
+});
+
+app.get("/", (_req, res) =>
+  res.json({
+    ok: true,
+    name: "node-arbitro-api",
+    version: "1.0.0",
+    containerId: os.hostname(), 
+  })
+);
+
+app.get("/health", (_req, res) => {
+  res.json({
+    ok: true,
+    containerId: os.hostname(), 
+    now: new Date().toISOString(),
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/arbitro", arbitroRoutes);
+app.use("/api/news", newsRoutes);
+
+
+app.use("/assets", express.static(path.join(__dirname, "../public")));
+
+
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    swaggerOptions: {
+      persistAuthorization: true,      
+      displayRequestDuration: true,
+    },
+    customJs: "/assets/swagger-pdf.js",
+  })
+);
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    ok: false,
+    error: "Not Found",
+    path: req.originalUrl,
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`[node-arbitro-api] Listening on http://localhost:${PORT}`);
+  console.log(`Swagger UI disponible en http://localhost:${PORT}/api/docs`);
 });
